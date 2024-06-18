b2f368faa663b16e87a2fda361c436fdbb470b13
eugene <eugsokolov@gmail.com>
2024-06-18 08:33:46 +0000

    format app.py

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ import base64
 import os
 import pickle
 
-os.environ["HF_DATASETS_OFFLINE"]="1"
-os.environ["TRANSFORMERS_OFFLINE"]="1"
+os.environ["HF_DATASETS_OFFLINE"] = "1"
+os.environ["TRANSFORMERS_OFFLINE"] = "1"
 
 import litserve as ls
 from fastapi.responses import JSONResponse
 
 from main import get_embeddings, get_model_from_ckpt, MODEL_CKPT
 
-AUTHORIZED_USERS = {"eugene", "szymon", "yc-collab",}
+AUTHORIZED_USERS = {
+    "eugene",
+    "szymon",
+    "yc-collab",
+}
 
 
 class ClayModelAPI(ls.LitAPI):
@@ -23,14 +27,19 @@ class ClayModelAPI(ls.LitAPI):
 
     def predict(self, request):
         if request["whoami"] not in AUTHORIZED_USERS:
-            return JSONResponse(status_code=401, content={"reason": "Please reach out to host to authorize access"})
+            return JSONResponse(
+                status_code=401,
+                content={"reason": "Please reach out to host to authorize access"},
+            )
 
         lat, lon = request["lat"], request["lon"]
         start, end = request["startdate"], request["enddate"]
         size = request.get("size", 256)
-        embeddings, stack = get_embeddings(lat, lon, start, end, size=size, model=self.model)
+        embeddings, stack = get_embeddings(
+            lat, lon, start, end, size=size, model=self.model
+        )
         pickled_array = pickle.dumps(embeddings)
-        base64_encoded_array = base64.b64encode(pickled_array).decode('utf-8')
+        base64_encoded_array = base64.b64encode(pickled_array).decode("utf-8")
         return {"output": base64_encoded_array}
 
 
